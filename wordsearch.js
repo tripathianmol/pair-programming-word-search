18f8a6e644265203e5a44caa7e0fc2b7baed081e
Anmol Tripathi <tripathianmol@protonmail.com>
2022-12-13 22:41:40 -0500

    Update wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+    
+  const verticalJoin = [];
+  for (let i = 0; i < letters.length; i++) {
+    let sentence = [];
+        
+    for (let vert of letters) {
+      sentence.push(vert[i]);
     }
-}
 
-module.exports = wordSearch
+    let joinedSentence = sentence.join("");
+    verticalJoin.push(joinedSentence);
+    console.log(verticalJoin);
+    sentence = [];
+  }
+
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  
+  for (let l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
